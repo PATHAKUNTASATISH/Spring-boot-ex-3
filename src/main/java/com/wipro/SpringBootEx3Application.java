8d6b0f01c6d64e673991d3454d270b5d7991ff91
PATHAKUNTASATISH <reddysatishkumar82@gmail.com>
2021-03-29 14:36:16 +0530

    added a method

--- src/main/java/com/wipro/SpringBootEx3Application.java
+++ src/main/java/com/wipro/SpringBootEx3Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootEx3Application {
     static int a=10;
+    public void show() {
+    	return ;
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEx3Application.class, args);
 	}
