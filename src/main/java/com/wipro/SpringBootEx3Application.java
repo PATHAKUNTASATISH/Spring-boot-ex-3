0e5e690b0fac43e20280706fbdd49cc2db88ba0c
SATISH KUMAR REDDY <58268432+PATHAKUNTASATISH@users.noreply.github.com>
2021-03-30 17:06:08 +0530

    Update SpringBootEx3Application.java

--- src/main/java/com/wipro/SpringBootEx3Application.java
+++ src/main/java/com/wipro/SpringBootEx3Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootEx3Application {
+	int e=5;
     static int a=10;
     public void show() {
     	return ;
